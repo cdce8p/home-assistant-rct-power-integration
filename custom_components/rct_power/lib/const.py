352ab58d0083b5b2e3a76c131734b16f9f0dc6d2
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2025-01-08 08:55:42 +0100

    Update constants

--- custom_components/rct_power/lib/const.py
+++ custom_components/rct_power/lib/const.py
@@ -2,34 +2,36 @@
 
 from __future__ import annotations
 
-# Base component constants
 from enum import KEEP, Enum, IntFlag, auto
+from typing import Final
 
-NAME = "RCT Power"
-DOMAIN = "rct_power"
-DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.14.2"
+from homeassistant.const import Platform
+
+NAME: Final = "RCT Power"
+DOMAIN: Final = "rct_power"
+VERSION: Final = "0.14.2"
 
 # Inverter
-INVERTER_MODEL = "RCT Power Storage"
+INVERTER_MODEL: Final = "RCT Power Storage"
 
 # Battery
-BATTERY_MODEL = "RCT Power Battery"
+BATTERY_MODEL: Final = "RCT Power Battery"
 
 # Icons
 ICON = "mdi:solar-power"
 
 # Platforms
-BINARY_SENSOR = "binary_sensor"
-SENSOR = "sensor"
-PLATFORMS = [SENSOR]
+PLATFORMS = [Platform.SENSOR]
 
+# Configuration and options
+CONF_ENABLED: Final = "enabled"
+CONF_HOSTNAME: Final = "hostname"
+CONF_PORT: Final = "port"
+CONF_SCAN_INTERVAL: Final = "scan_interval"
 
 # Defaults
-DEFAULT_NAME = DOMAIN
-
-NUMERIC_STATE_DECIMAL_DIGITS = 1
-FREQUENCY_STATE_DECIMAL_DIGITS = 3
+NUMERIC_STATE_DECIMAL_DIGITS: Final = 1
+FREQUENCY_STATE_DECIMAL_DIGITS: Final = 3
 
 
 class EntityUpdatePriority(Enum):
