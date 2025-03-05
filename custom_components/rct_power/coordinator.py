7f1829c699140d876c964688e392fc23f03dfea9
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2025-03-05 21:41:47 +0100

    Update coordinator

--- custom_components/rct_power/coordinator.py
+++ custom_components/rct_power/coordinator.py
@@ -2,29 +2,38 @@ from __future__ import annotations
 
 from datetime import timedelta
 
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-from ..const import LOGGER
-from .api import ApiResponseValue, RctPowerApiClient, RctPowerData, ValidApiResponse
+from .const import LOGGER
+from .lib.api import ApiResponseValue, RctPowerApiClient, RctPowerData, ValidApiResponse
+from .lib.const import DOMAIN
 
 
 class RctPowerDataUpdateCoordinator(DataUpdateCoordinator[RctPowerData]):
     """Class to manage fetching data from the rct power inverter API."""
 
+    config_entry: ConfigEntry
+
     def __init__(
         self,
         hass: HomeAssistant,
-        name: str,
+        entry: ConfigEntry,
+        *,
+        name_suffix: str,
         client: RctPowerApiClient,
         object_ids: list[int],
         update_interval: timedelta | None = None,
     ) -> None:
         self.client = client
         self.object_ids = object_ids
-
         super().__init__(
-            hass=hass, logger=LOGGER, name=name, update_interval=update_interval
+            hass=hass,
+            config_entry=entry,
+            logger=LOGGER,
+            name=f"{DOMAIN} {entry.unique_id} {name_suffix}",
+            update_interval=update_interval,
         )
 
     def get_latest_response(self, object_id: int):
